d2bb5187cc349964c5403076eb07c84e5e3b1995
mintmarshmallow <nopp054@naver.com>
2019-09-07 18:01:01 +0900

    19/09/07 last update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import style from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
